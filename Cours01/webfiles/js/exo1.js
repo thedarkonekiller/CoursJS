5856196dee7a5c3cd7d8c78453ad47cca0658307
thedarkonekiller <76535453+thedarkonekiller@users.noreply.github.com>
2023-05-22 15:55:40 +0200

    javascript first course

---
+++ Cours01/webfiles/js/exo1.js
@@ -0,0 +1,82 @@
+// alert("bonjour");
+
+// let variable1 = null
+
+// if (variable1 != null) {
+
+// console.log(variable1)
+
+// } else {
+
+//     console.log("Variable null")
+
+// }
+
+// let num = -10
+
+// if (num > 0) {
+//     // On utilise var car on va utiliser la variable en dehors du contexte (la condition)
+//     var result = "Supérieur à 0"
+// } else {
+//     if (num === 0) {
+//         // On utilise var car on va utiliser la variable en dehors du contexte (la condition)
+//         var result = "Nul"
+        
+//     } else {
+//         // On utilise var car on va utiliser la variable en dehors du contexte (la condition)
+//         var result = "Inférieur à 0" 
+        
+//     }
+// }
+// console.log(result)
+
+// let num2 = Number(window.prompt("Entrer un chiffre"));
+
+// if (num > 0) {
+//     document.write("Supérieur à 0")
+// } else {
+//     if (num === 0) {
+//         document.write("Nul")
+        
+//     } else {
+//         document.write("Inférieur à 0")
+        
+//     }
+// }
+
+
+// On peut utiliser var ou const mais let est préfèrable pour optimiser la mémoire
+// let b = 25
+
+// if (b > 0) {
+//     console.log(b)
+// }
+
+
+// for (let i = 0; i < 11; i++) {
+//     console.log("5 x " + i + " = " + 5*i);
+    
+// }
+
+// for (let i = 0; i < 11; i++) {
+//     // on utilise let car on veut uniquement utiliser result dans la boucle
+//     let result = 5*i
+//     console.log("5 x " + i + " = " + result);
+    
+// }
+
+
+// const num = Number(window.prompt("Entrer un chiffre"));
+
+// for (let i = 0; i < 11; i++) {
+//     document.write('<p>', num + " x " + i + " = " + num*i, '</p>');
+    
+// }
+
+for (let i = 0; i < 11; i++) {
+    for (let f = 0; f < 11; f++) {
+        let result = i*f
+        console.log(i + " x " + f + " = " + result);
+    }
+    
+}
