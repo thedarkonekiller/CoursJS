9096fbd80baab3e8cb434a7b567cb1d050d4f44c
thedarkonekiller <76535453+thedarkonekiller@users.noreply.github.com>
2023-05-25 11:55:52 +0200

    maj

--- Cours03/Exo_1 (Initiation)/script.js
+++ Cours03/Exo_1 (Initiation)/script.js
@@ -21,8 +21,10 @@ buttonMb.addEventListener("click", bigger)
 function bigger(){
     if(img.src.match("small.jpg")){
         img.src = "./src/img/big.jpg"
+        buttonMb.textContent = "Réduire l'image"
     } else {
         img.src = "./src/img/small.jpg"
+        buttonMb.textContent = "Agrandir l'image"
     }
 }
 
@@ -34,8 +36,10 @@ darkMode.addEventListener("click", toggleDarkMode)
 function toggleDarkMode(){
     if(bdy.classList.contains("darkModeIsActive")){
         bdy.classList.remove("darkModeIsActive")
+        darkMode.textContent = "MODE SOMBRE"
     }else{
         bdy.classList.add("darkModeIsActive")
+        darkMode.textContent = "MODE CLAIR"
     }
 }
 
@@ -52,8 +56,10 @@ let linkShmore = document.querySelector(".loadMore")
 linkShmore.addEventListener("click", () => {
     if (shwMore.style.display === "none") {
         shwMore.style.display = "block"
+        linkShmore.textContent = "Afficher -"
     } else {
         shwMore.style.display = "none"
+        linkShmore.textContent = "Afficher +"
     }
 })
 let text = document.querySelector("#seText")
@@ -67,3 +73,51 @@ dspOnPage.addEventListener("click", () => {
         alert("Veuillez entrer un texte !")
     }
 })
+
+// On séléctionne toutes les cellules
+let allCells = document.querySelectorAll("td");
+// On ajoute sur chaque cellule un EventListener de type "click"
+// au click sur la cellule on execute la fonction colorToggle
+allCells.forEach(cell => {
+    cell.addEventListener("click", colorToggle);
+});
+/**
+ * Permet de colorer le fond de la cellule
+ * en bleu ou rouge sans que 2 cellules
+ * collées ne soient de la même couleur
+ */
+function colorToggle() {
+    // On teste si la cellule contient la classe "blue" ou la classe "red"
+    if (this.classList.contains("blue") || this.classList.contains("red")) {
+        // Si oui alors on affiche une alerte
+        alert("Déjà cliqué");
+    } else {
+        //On défini des variables qui sélectionne les cellules qui entourent la cellule actuelle
+        let previousRowCell = this.parentNode.previousElementSibling;
+        let nextRowCell = this.parentNode.nextElementSibling;
+        let previousColumnCell = this.previousElementSibling;
+        let nextColumnCell = this.nextElementSibling;
+        
+        // On teste si :
+        // - la cellule de la colonne précèdente n'existe pas OU qu'elle contient la classe "blue"
+        // ET QUE
+        // - la cellule de la colonne suivante n'existe pas OU qu'elle contient la classe "blue"
+        // ET QUE
+        // - la cellule de la ligne précèdente n'existe pas OU qu'elle contient la classe "blue"
+        // ET QUE
+        // - la cellule de la ligne suivante n'existe pas OU qu'elle contient la classe "blue"
+        // SI OUI
+        // On ajoute la classe "blue"
+        // SINON
+        // On ajoute la classe "red"
+
+        if ((!previousColumnCell || !previousColumnCell.classList.contains("blue")) &&
+            (!nextColumnCell || !nextColumnCell.classList.contains("blue")) &&
+            (!previousRowCell || !previousRowCell.children[this.cellIndex].classList.contains("blue")) &&
+            (!nextRowCell || !nextRowCell.children[this.cellIndex].classList.contains("blue"))) {
+            this.classList.add("blue");
+        } else {
+            this.classList.add("red");
+        }
+    }
+}
